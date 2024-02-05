d737debcb3760dce07fc9a9ccf9edc3d29d87b16
Ihsan Fadli Tampati <96326397+Ihsanfati@users.noreply.github.com>
2024-02-05 23:20:05 +0700

    python test xss.php

--- tests/otomasi-xss-selenium.py
+++ tests/otomasi-xss-selenium.py
@@ -1,72 +1,45 @@
-import unittest
+import unittest, os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-class XSSPageTest(unittest.TestCase):
+class XSSDetectionTestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.browser = webdriver.Chrome()
-        cls.browser.get("http://localhost/ihsanfati-uas-ppl-BadCRUD/login.php")
+        option = webdriver.FirefoxOptions()
+        option.add_argument('--headless')
+        cls.browser = webdriver.Firefox(options=option)
+        try:
+            cls.url = os.environ['URL']
+        except:
+            cls.url = "http://localhost"
+            
+    def test(self):
+        self.login_correct_credentials()
+        self.xss_page()
 
-    def login(self):
-        username_input = self.browser.find_element(By.ID, "inputUsername")
-        password_input = self.browser.find_element(By.ID, "inputPassword")
-        login_button = self.browser.find_element(By.CSS_SELECTOR, "[type='submit']")
+    def login_correct_credentials(self):
+        login_url = self.url + '/login.php'
+        self.browser.get(login_url)
 
-        username_input.send_keys("admin")
-        password_input.send_keys("nimda666!")
-        login_button.click()
+        self.browser.find_element(By.ID, 'inputUsername').send_keys('admin')
+        self.browser.find_element(By.ID, 'inputPassword').send_keys('nimda666!')
+        self.browser.find_element(By.TAG_NAME, 'button').click()
 
-        WebDriverWait(self.browser, 10).until(EC.title_contains("Dashboard"))
+    def xss_page(self):
+        xss_url = self.url + '/xss.php'
+        self.browser.get(xss_url)
 
-        dashboard_heading = self.browser.find_element(By.XPATH, "//h2[contains(text(), 'Halo, admin')]")
-        self.assertTrue(dashboard_heading.is_displayed())
+        input_field = self.browser.find_element(By.NAME, 'thing')
+        input_value = '<script>alert("Hacked by Fati!");</script>'
+        input_field.send_keys(input_value)
 
-    def test_step_1_to_3(self):
-        self.login()
-
-    def test_step_4_to_6(self):
-        xss_button = WebDriverWait(self.browser, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//a[contains(text(), 'XSS')]"))
-        )
-        xss_button.click()
-
-        xss_title = WebDriverWait(self.browser, 10).until(
-            EC.title_contains("XSS")
-        )
-        self.assertTrue(WebDriverWait(self.browser, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//title[contains(text(), 'XSS')]"))
-        ).is_displayed())
-
-    def test_step_9_to_11(self):
-        dashboard_button = WebDriverWait(self.browser, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//a[contains(text(), 'Dashboard')]"))
-        )
-        sign_out_button = WebDriverWait(self.browser, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//a[contains(text(), 'Sign out')]"))
-        )
-
-        self.assertTrue(dashboard_button.is_displayed())
-        self.assertTrue(sign_out_button.is_displayed())
-
-        thing_input = self.browser.find_element(By.NAME, "thing")
-        submit_button = self.browser.find_element(By.NAME, "submit")
-
-        self.assertTrue(thing_input.is_displayed())
-        self.assertTrue(submit_button.is_displayed())
-
-        # Try to write "hacked by ihsanfati" in "thing" input
-        thing_input.send_keys("hacked by ihsanfati")
+        submit_button = self.browser.find_element(By.NAME, 'submit')
         submit_button.click()
 
-        # Verify that "Your thing is hacked by ihsanfati" is visible successfully
-        result_message = WebDriverWait(self.browser, 3).until(
-            EC.visibility_of_element_located((By.XPATH, "//p[contains(text(), 'Your thing is hacked by ihsanfati')]"))
-        )
-        self.assertTrue(result_message.is_displayed())
+        alert = self.browser.switch_to.alert
+        self.assertEqual('Hacked by Fati!', alert.text)
+        alert.accept()
 
     @classmethod
     def tearDownClass(cls):
